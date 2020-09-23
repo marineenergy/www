989db237fb42c15eae3fb69ee7af1aba535d00a3
Ben Best <ben@ecoquants.com>
2020-09-23 22:04:11 +0000

    regs_template.Rmd -> _regs_template.Rmd

--- render_diagrams.R
+++ render_diagrams.R
@@ -6,7 +6,7 @@ mmd_files <- list.files("diagrams", "\\.mmd$")
 for (mmd in mmd_files){ # mmd = mmd_files[1]
   html <- paste0("regs_", path_ext_set(mmd, "html"))
   render(
-    "regs_template.Rmd", 
+    "_regs_template.Rmd", 
     output_file = html,
     params = list(mmd = mmd))
 }
